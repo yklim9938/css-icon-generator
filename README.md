3ad4d367b03e75cc25e9fc8f5e753f005268b7f0
yklim9938 <yk9938@gmail.com>
2024-06-19 14:37:36 +0800

    firebase

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # CSS Icons Generator
 
 Combine svg and png files into single css file.
+
+Web App: [https://css-icon-generator.web.app/](https://css-icon-generator.web.app/)
--- plugins/firebase.client.js
+++ plugins/firebase.client.js
@@ -1,20 +1,19 @@
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
+// import { getAnalytics } from "firebase/analytics";
 
 export default defineNuxtPlugin(nuxtApp => {
-    /* nuxtApp.hook('app:mounted', () => {
+    nuxtApp.hook('app:mounted', () => {
         const firebaseConfig = {
-            apiKey: "AIzaSyBLwW-yvEGHwUzsJe-WYWcje1QSDSwBGmY",
-            authDomain: "itu-siapa.firebaseapp.com",
-            projectId: "itu-siapa",
-            storageBucket: "itu-siapa.appspot.com",
-            messagingSenderId: "1021171214545",
-            appId: "1:1021171214545:web:b86f688987716801a7fdf0",
-            measurementId: "G-2CBEY2HBTT"
-          };
+            apiKey: "AIzaSyAPK9tC7VvuuhhoSrEVGVSS7C8OLejxKSg",
+            authDomain: "css-icon-generator.firebaseapp.com",
+            projectId: "css-icon-generator",
+            storageBucket: "css-icon-generator.appspot.com",
+            messagingSenderId: "947150508341",
+            appId: "1:947150508341:web:e8a5487de8b68f3609fc7c"
+        };
           
         // Initialize Firebase
         const app = initializeApp(firebaseConfig);
-        const analytics = getAnalytics(app);
-    }) */
+        // const analytics = getAnalytics(app);
+    })
 })
