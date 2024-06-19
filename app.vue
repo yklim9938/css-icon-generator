a0cebfd3776b59ccf8efc51f31d9c334de2f659e
yklim9938 <yk9938@gmail.com>
2024-06-19 12:03:21 +0800

    support fixed svg colors

--- app.vue
+++ app.vue
@@ -4,6 +4,13 @@
         <h1 class="text-center pt-6 text-3xl sm:text-4xl font-bold text-primary">CSS Icons Generator</h1>
         <h2 class="text-center pt-4 font-medium text-sm sm:text-base">Generate css icons with your svg and png files.</h2>
         <div class="w-full max-w-[600px] mx-auto mt-8">
+          <div class="flex gap-2 justify-end mb-2">
+            <div class="tooltip-container relative">
+              <div class="tooltip-info inline-block absolute bottom-[100%] right-0 w-[240px] bg-content-light py-1 px-2 rounded-md shadow-sm text-xs">Check this if your svg has fixed colors</div>
+              <svg class="mt-[1px] text-primary-light cursor-pointer" xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24"><path fill="currentColor" d="M11 17h2v-6h-2zm1-8q.425 0 .713-.288T13 8t-.288-.712T12 7t-.712.288T11 8t.288.713T12 9m0 13q-2.075 0-3.9-.788t-3.175-2.137T2.788 15.9T2 12t.788-3.9t2.137-3.175T8.1 2.788T12 2t3.9.788t3.175 2.137T21.213 8.1T22 12t-.788 3.9t-2.137 3.175t-3.175 2.138T12 22"/></svg>
+            </div>
+            <Checkbox switch reverse v-model="staticSvg"><span class="text-sm">Static SVG</span></Checkbox>
+          </div>
           <PhotoDrop v-if="step == 1" @input="generateCSS" accept="image/png, image/svg+xml"/>
         </div>
         <p class="mt-6 text-xs sm:text-sm font-medium text-center !leading-loose">*Make sure to rename your files with <b>small caps and hyphen</b> only</p>
@@ -37,21 +44,32 @@ useSeoMeta({
 
 const step = ref(1)
 
+const staticSvg = ref(false)
 const svgToCSS = (className, svgText) => {
   let cssText = svgText.replace(/<\!--.*?-->/g, "") // remove comments
   cssText = cssText.replace(/(\r\n|\n|\r)/gm, "") // remove line breaks
   cssText = cssText.replace(/version=".*?"/g, "") // remove version
   cssText = cssText.replace(/width=".*?"/g, "") // remove width
   cssText = cssText.replace(/height=".*?"/g, "") // remove height
-  // cssText = cssText.replace(/fill=".*?"/g, 'fill="currentColor"') // change fill to currentColor
-  // cssText = cssText.replace(/stroke=".*?"/g, 'fill="currentColor"') // change fill to currentColor
+  if (!staticSvg.value) {
+    cssText = cssText.replace(/fill=".*?"/g, 'fill="currentColor"') // change fill to currentColor
+    cssText = cssText.replace(/stroke=".*?"/g, 'fill="currentColor"') // change fill to currentColor
+  }
   cssText = cssText.replace(/#/g, '%23')
   cssText = cssText.replace(/</g, '%3C')
   cssText = cssText.replace(/>/g, '%3E')
   cssText = cssText.replace(/"/g, "'")
-  cssText = `i.icon-${className} {
-  --svg: url("data:image/svg+xml,${cssText}");
+  if (!staticSvg.value) {
+    cssText = `i.icon-${className} {
+    --svg: url("data:image/svg+xml,${cssText}");
 }`
+  }
+  else {
+    cssText = `i.icon-${className} {
+  background-color: unset;
+  background-image: url("data:image/svg+xml,${cssText}");
+}`
+  }
 
   return cssText
 }
@@ -234,5 +252,17 @@ const copyCode = async () => {
 
 .code-area {
   resize: none;
+}
+
+.tooltip-info {
+  visibility: hidden;
+  opacity: 0;
+  transition: all 0.15s;
+  pointer-events: none
+}
+
+.tooltip-container:hover .tooltip-info {
+  visibility: visible;
+  opacity: 1;
 }
 </style>
