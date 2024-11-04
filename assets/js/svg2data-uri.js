5441a51e9ce0d2c58cdbd095b286f6ff503ad322
yklim9938 <yk9938@gmail.com>
2024-11-04 21:42:55 +0800

    bug fix

--- assets/js/svg2data-uri.js
+++ assets/js/svg2data-uri.js
@@ -1,20 +1,20 @@
 const svg2DataURI = (svgCode) => {
-    let dataURI = svgCode.replace(/<\!--.*?-->/g, "") // remove comments
-    dataURI = dataURI.replace(/(\r\n|\n|\r)/gm, "") // remove line breaks
+    let dataURI = svgCode
+    const extractedSvg = svgCode.match(/<svg[\S\s]*>/)
+    if (extractedSvg[0]) {
+        dataURI = extractedSvg[0]
+    }
+    dataURI = dataURI.replace(/<metadata[\s\S]*metadata>/g, "") // remove metadata
+    dataURI = dataURI.replace(/<\!--.*?-->/g, "") // remove comments
+    dataURI = dataURI.replace(/(\r\n|\n|\r)/gm, " ") // replace line breaks with space
     dataURI = dataURI.replace(/\sversion=".*?"/g, "") // remove version
     dataURI = dataURI.replace(/\s{2,}/g, " ") // replace long spaces with single space
-    // dataURI = dataURI.replace(/\swidth=".*?"/g, "") // remove width
-    // dataURI = dataURI.replace(/\sheight=".*?"/g, "") // remove height
-    /*   if (!staticSvg.value) {
-        dataURI = dataURI.replace(/fill=".*?"/g, 'fill="currentColor"') // change fill to currentColor
-        dataURI = dataURI.replace(/stroke=".*?"/g, 'stroke="currentColor"') // change fill to currentColor
-      } */
     dataURI = dataURI.replace(/#/g, '%23')
     dataURI = dataURI.replace(/</g, '%3C')
     dataURI = dataURI.replace(/>/g, '%3E')
     dataURI = dataURI.replace(/"/g, "'")
     dataURI = "data:image/svg+xml," + dataURI
     return dataURI
-} 
+}
 
 export default svg2DataURI
--- pages/data-uri.vue
+++ pages/data-uri.vue
@@ -6,7 +6,7 @@
                 <textarea v-model="codeInput" class="border border-line rounded min-h-[150px] w-full resize-none py-2 px-3 outline-none text-xs" placeholder="Paste your svg code here"></textarea>
                 <div class="mt-4">
                     <div class="flex gap-4 flex-col sm:flex-row">
-                        <div class="size-[150px] mx-auto flex-shrink-0 border border-line rounded bg-center bg-cover bg-no-repeat flex items-center justify-center" :style="preview">
+                        <div class="size-[150px] mx-auto flex-shrink-0 border border-line rounded bg-center bg-contain bg-no-repeat flex items-center justify-center" :style="preview">
                             <div v-if="!preview" class="text-xs text-gray-400">Preview</div>
                         </div>
                         <textarea class="bg-content-light rounded border border-light outline-none p-2 text-xs h-[150px] resize-none w-full" disabled placeholder="Output Data URI will be here">{{ dataURI }}</textarea>
